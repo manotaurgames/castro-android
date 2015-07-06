19e9fb425a75e86ff49e217d352a4b262f5a0283
Aaron Sarazan <aaron@levelmoney.com>
2015-07-05 18:43:13 -0700

    Update copyright

--- sample/src/main/java/com/manotaurgames/castro/sample/MainActivity.java
+++ sample/src/main/java/com/manotaurgames/castro/sample/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Manotaur LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.manotaurgames.castro.sample;
 
 import android.content.Intent;
@@ -20,10 +36,6 @@ import com.manotaurgames.castro.CastActivity;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by Aaron Sarazan on 7/4/15
- * Copyright(c) 2015 Level, Inc.
- */
 public class MainActivity extends CastActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
--- sample/src/main/java/com/manotaurgames/castro/sample/MainFragment.java
+++ sample/src/main/java/com/manotaurgames/castro/sample/MainFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Manotaur LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.manotaurgames.castro.sample;
 
 import android.app.Activity;
@@ -24,18 +40,12 @@ import com.manotaurgames.castro.CastFragment;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by Aaron Sarazan on 7/4/15
- * Copyright(c) 2015 Level, Inc.
- */
 public class MainFragment extends CastFragment {
 
     private static final String TAG = MainFragment.class.getSimpleName();
     private static final int REQUEST_CODE = 1;
 
     private HelloWorldChannel mHelloWorldChannel;
-
-
 
     @Nullable
     @Override
